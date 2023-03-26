1396026159b4db84d658668add215fdcc3b8ef78
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2023-03-25 19:00:24 -0700

    Update script.js
    Tried updating Validation but failed. Cleaned up project to ask help on Slack

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ jobTitle.addEventListener('change', (e) => {
     };
 });
 
-
 /**
  * First, the "Color" dropdown menu is disabled until an option is 
    selected from the "Design" dropdown menu.
@@ -31,7 +30,6 @@ jobTitle.addEventListener('change', (e) => {
 const design = document.querySelector('#design');
 const color = document.querySelector('#color');
 color.disabled = true;
-
 
 design.addEventListener('change', (e) => {
     color.disabled = false;
@@ -51,7 +49,6 @@ design.addEventListener('change', (e) => {
     }
 });
 
-
 /**
  * The totalcost is updated according to how many workshop checkboxes
    the user selects. 
@@ -70,7 +67,6 @@ activities.addEventListener('change', e => {
     };
     totalDisplay.innerHTML = `Total: $${totalCost}`
 });
-
 
 /**
  * Make the "Credit Card" option the default display option when 
@@ -93,12 +89,12 @@ paypal.style.display = 'none';
 
 payment.addEventListener('change', e => {
     const value = e.target.value;
+    
     if (value === 'credit-card') {
         creditcard.style.display = '';
         expirationBox.style.display = '';
         bitcoin.style.display = 'none';
         paypal.style.display = 'none';
-
     } else if (value === 'paypal') {
         creditcard.style.display = 'none';
         expirationBox.style.display = 'none';
@@ -109,5 +105,176 @@ payment.addEventListener('change', e => {
         expirationBox.style.display = 'none';
         bitcoin.style.display = '';
         paypal.style.display = 'none';
-    }
+    };
 });
+
+
+/**
+ * Program the form element to listen for the submit event. When the form submission is 
+   detected, each required form field or section should be validated, or checked to ensure 
+   that they have been filled out correctly. If any of the following required fields is not 
+   valid, the form’s submission should be prevented.
+ * The "Name" field cannot be blank or empty.
+ * The "Email Address" field must contain a validly formatted email address. The email address 
+   does not need to be a real email address, just formatted like one. For example: 
+   dave@teamtreehouse.com. A few characters for the username, followed by "@", followed by 
+   a few more characters and a ".com" for the domain name. You don’t have to account for 
+   other top-level domains, like .org, .net, etc.
+ * The "Register for Activities" section must have at least one activity selected.
+ * If and only if credit card is the selected payment method:
+     * The "Card number" field must contain a 13 - 16 digit credit card number with no dashes 
+       or spaces. The value does not need to be a real credit card number.
+     * The "Zip code" field must contain a 5 digit number.
+     * The "CVV" field must contain a 3 digit number.
+ * Project Warm Up: For some experience with the techniques you’ll use in this section, 
+   complete this short exercise - Form Input Validation.
+ * Note:
+     * Avoid using snippets, libraries or plugins.
+     * Only validate the three credit card fields if "credit card" is the selected payment option.
+     * Only call `preventDefault` on the `event` object if one or more of the required fields is 
+       invalid.
+ * Pro Tip:A recommended approach is to create helper functions for each of the required 
+    fields to be validated. For example, for the "Name" field, a function could check the 
+    "Name" field’s value. If it equals an empty string or only blank spaces, the function 
+    could log out a helpful statement and return false. Otherwise it would return true. And 
+    then in the `submit` event listener, you could call that helper function and check what 
+    it returns: if it returns false, you would prevent the form from submitting. Otherwise, 
+    you would avoid preventing form submission, and allow the `submit` handler to either 
+    submit or move onto checking the next required field.
+ */
+
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log(nameValidator());
+    if(nameValidator() === false) {
+        e.preventDefault();
+    };
+
+    console.log(emailValidator());
+    if(emailValidator() === false) {
+        e.preventDefault();
+    };
+
+    console.log(activitiesValidator());
+    if(activitiesValidator() === false) {
+        e.preventDefault();
+    };
+
+});
+
+function nameValidator() {
+    const nameValue = name.value;
+    const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
+    return nameIsValid;
+};
+
+function emailValidator() {
+    const emailValue = email.value;
+    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+    return emailIsValid;
+};
+
+function activitiesValidator() {
+    const activitiesIsValid = totalCost > 0;
+    return activitiesIsValid;
+};
+
+/************************************************************************/
+
+/***
+ * I'm having trouble validating the credit card information. Specifically, I am trying to test the CC number, ZipCode, and CVV
+ * only if the Credit Card option is selected from the Payment Info dropdown menu. I've tried logging out `payment.selectedIndex` to 
+ * select the Credit Card option. However, every time I try using `.selectedIndex`, it always selects the Credit Card option 
+ * regardless of which options is selected. Even when the paypal or bitcoin options are selected, the `selectedIndex` still logs
+ * to Credit Card. 
+ * I've also tried adding a `selected` attribute to the selected option using the `payment.addEventListener`. But for some reason,
+ * when I log out the `payment.selectedIndex` that way, it works if I log it out within the EventListener. But if I try to log it
+ * in the form submit listener, it displays undefined.
+ * 
+ */
+
+
+
+
+
+
+/*
+const ccOption = document.querySelector('[value="credit-card"]');
+const paypalOption = document.querySelector('[value="paypal"]');
+const bitcoinOption = document.querySelector('[value="bitcoin"]');
+
+    paypalOption.setAttribute('selected', false);
+    bitcoinOption.setAttribute('selected', false);
+    ccOption.setAttribute('selected', 'selected');
+
+    ccOption.selected = true;
+    paypalOption.selected = false;
+    bitcoinOption.selected = false;
+
+    ccOption.selected = false;
+    paypalOption.selected = true;
+    bitcoinOption.selected = false;
+
+    ccOption.selected = false;
+    paypalOption.selected = false;
+    bitcoinOption.selected = true;
+
+
+const ccNum = document.querySelector('#cc-num');
+const zipCode = document.querySelector('#zip');
+const cvvNum = document.querySelector('#cvv');
+
+const selectedPaymentIndex = payment.selectedIndex;
+console.log(selectedPaymentIndex);
+
+
+if (selectedPaymentIndex === 1) {
+    console.log(ccNumValidator());
+    if(ccNumValidator() === false) {
+        e.preventDefault();
+    };
+};
+if (selectedPaymentOption == 'credit-card') {
+    console.log(ccZipCodeValidator());
+    if(ccZipCodeValidator() === false) {
+        e.preventDefault();
+    };
+};
+
+if (selectedPaymentOption == 'credit-card') {
+    console.log(cvvValidator());
+    if(cvvValidator() === false) {
+        e.preventDefault();
+    };
+} else {
+    return true;
+};
+
+
+function ccNumValidator() {
+    const ccNumberValue = ccNum.value
+    const cardNumberIsValid = /^\d{13,16}$/.test(ccNumberValue);
+    return cardNumberIsValid;
+
+};
+
+function ccZipCodeValidator() {
+    const zipValue = zipCode.value
+    if (selectedPaymentOption === 'credit-card') {
+        const zipIsValid = /^\d{5}$/.test(zipValue);
+        return zipIsValid;
+    };
+};
+
+function cvvValidator() {
+    const cvvValue = cvvNum.value
+    if (selectedPaymentOption === 'credit-card') {
+        const cvvIsValid = /^\d{3}$/.test(cvvValue);
+        return cvvIsValid;
+    };
+};
+*/
